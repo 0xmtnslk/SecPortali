a42efc0983132c8ee4df0b1b304d213a0bcc1b34
0xmtnslk <artemissdao@gmail.com>
2026-03-29 19:22:33 +0300

    Vite: allowedHosts eklendi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,11 +9,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://backend:3001',
-        changeOrigin: true,
-      }
-    }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['sec-portali.medicalisg.com']
   }
 })
